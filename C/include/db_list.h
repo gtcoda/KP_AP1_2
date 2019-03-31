8e574c60726b481687f1483d50f549e4150a884d
gtcoda <gtcoda@gmail.com>
2019-03-31 09:31:40 +0300

    Рабочая версия с хранением в связном списке.

--- C/include/db_list.h
+++ C/include/db_list.h
@@ -11,7 +11,6 @@ uint8_t read_db_list(rn_t number, record_t * note, record_db_l *list);
 uint8_t write_db_list(record_t note, record_db_l * list, rn_t * number);
 uint8_t write_db_list_sort(record_t note, record_db_l * list, rn_t * number, sort_t column);
 
-
 rn_t amount_db_list(record_db_l * list);
 
 uint8_t replace_db_list(record_t note, record_db_l * list, rn_t number);
